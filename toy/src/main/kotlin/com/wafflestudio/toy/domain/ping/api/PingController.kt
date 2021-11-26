df2eec740862c859f35540ff90235130297458cb
nemga123 <hspark0419@naver.com>
2021-11-26 22:27:59 +0900

    Feat : #8 frontend Port access @CrossOrigin annotation 추가 url 변경

--- toy/src/main/kotlin/com/wafflestudio/toy/domain/ping/api/PingController.kt
+++ toy/src/main/kotlin/com/wafflestudio/toy/domain/ping/api/PingController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-@CrossOrigin("https://localhost:3000")
+@CrossOrigin("http://velog-toyproject-team7-web.s3-website.ap-northeast-2.amazonaws.com/")
 @RestController
 class PingController {
     @GetMapping("/ping/")

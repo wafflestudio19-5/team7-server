2062eba726f49e26d2146b2e3309b63adfba348b
nemga123 <hspark0419@naver.com>
2021-11-26 22:17:07 +0900

    Feat : #8 frontend Port access @CrossOrigin annotation 추가

--- toy/src/main/kotlin/com/wafflestudio/toy/domain/ping/api/PingController.kt
+++ toy/src/main/kotlin/com/wafflestudio/toy/domain/ping/api/PingController.kt
@@ -1,9 +1,11 @@
 package com.wafflestudio.toy.domain.ping.api
 
 import com.wafflestudio.toy.domain.ping.dto.PingDto
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin("https://localhost:3000")
 @RestController
 class PingController {
     @GetMapping("/ping/")

ef78b24f37d82121c2938dff36c844886d12dca8
WallabyG <ycm1366@gmail.com>
2021-12-20 03:44:24 +0900

    chore: Add no-arg plugin to gradle config

--- toy/build.gradle.kts
+++ toy/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.0"
     kotlin("plugin.jpa") version "1.6.0"
+    kotlin("plugin.noarg") version "1.6.0"
 }
 
 group = "com.wafflestudio"

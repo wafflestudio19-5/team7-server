de5cc6780172258190a2759fb8d2f0b89c8dcf6b
WallabyG <ycm1366@gmail.com>
2021-12-21 05:12:41 +0900

    feat: #15 - Add models for each domain
    Created models based on DB schema
    Comment uses nested set model

---
+++ toy/src/main/kotlin/com/wafflestudio/toy/domain/post/model/Comment.kt
@@ -0,0 +1,34 @@
+package com.wafflestudio.toy.domain.post.model
+
+import com.wafflestudio.toy.domain.model.BaseTimeEntity
+import com.wafflestudio.toy.domain.user.model.User
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
+@Entity
+class Comment (
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    val user: User?,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", referencedColumnName = "id")
+    val post: Post,
+
+    val rootComment: Long,
+    val lft: Long,
+    val rgt: Long,
+
+    @field:Min(0)
+    val depth: Int,
+
+    @field:NotBlank
+    val content: String,
+
+    ): BaseTimeEntity()

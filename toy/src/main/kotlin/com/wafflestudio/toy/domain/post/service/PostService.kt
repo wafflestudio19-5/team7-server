dc1d5ffa34b3609f3ed8384221c64df56ff51c31
WallabyG <ycm1366@gmail.com>
2021-12-26 22:42:29 +0900

    feat: Add search post API & modify post API response
    APIs of post domain now returns Page instead of List
    Page provides useful info for pagination

--- toy/src/main/kotlin/com/wafflestudio/toy/domain/post/service/PostService.kt
+++ toy/src/main/kotlin/com/wafflestudio/toy/domain/post/service/PostService.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.toy.domain.post.service
 import com.wafflestudio.toy.domain.post.dto.PostDto
 import com.wafflestudio.toy.domain.post.model.Post
 import com.wafflestudio.toy.domain.post.repository.PostRepository
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
@@ -13,15 +14,20 @@ import java.time.LocalDate
 class PostService(
     private val postRepository: PostRepository
 ) {
-    fun getRecentPosts(pageable : Pageable): List<PostDto.MainPageResponse> {
-        val posts: List<Post> =
+    fun getRecentPosts(pageable : Pageable): Page<PostDto.MainPageResponse> {
+        val posts: Page<Post> =
             postRepository.findAllByPrivateIsFalse(pageable)
         return posts.map { post -> PostDto.MainPageResponse(post) }
     }
 
-    fun getTrendingPosts(pageable: Pageable, date: Int): List<PostDto.MainPageResponse> {
+    fun getTrendingPosts(pageable: Pageable, date: Int): Page<PostDto.MainPageResponse> {
         val dateStart = LocalDate.now().atStartOfDay().minusDays(date.toLong())
         val posts = postRepository.findAllByPrivateIsFalseAndCreatedAtAfter(pageable, dateStart)
+        return posts.map { post -> PostDto.MainPageResponse(post) }
+    }
+
+    fun searchPosts(pageable: Pageable, keyword: String): Page<PostDto.MainPageResponse> {
+        val posts = postRepository.searchByKeyword(pageable, keyword, keyword, keyword)
         return posts.map { post -> PostDto.MainPageResponse(post) }
     }
 }

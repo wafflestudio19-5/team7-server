0052ac60ee065f94f2a609752f19a2eb0423f0b0
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-01-03 20:04:53 +0900

    bugs: RestController setting

--- waflog/src/main/kotlin/com/wafflestudio/waflog/global/auth/api/AuthController.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/global/auth/api/AuthController.kt
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@RestController
 @RequestMapping("/api/v1/auth")
 class AuthController(
     private val authService: AuthService,

6a0b644c765457f795f8fff004d7258d259b9a3d
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-01-07 00:01:33 +0900

    [#64] add param annotation

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/user/repository/SeriesRepository.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/user/repository/SeriesRepository.kt
@@ -4,10 +4,11 @@ import com.wafflestudio.waflog.domain.user.model.Series
 import com.wafflestudio.waflog.domain.user.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface SeriesRepository : JpaRepository<Series, Long?> {
     fun findByName(name: String): Series?
 
-    @Query("SELECT s FROM Series s WHERE s.name =: name AND s.user =: user")
-    fun findByNameAndUser(name: String, user: User): Series?
+    @Query("SELECT s FROM Series s WHERE s.name = :name AND s.user = :user")
+    fun findByNameAndUser(@Param("name") name: String, @Param("user") user: User): Series?
 }

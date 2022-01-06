c23a0040d0b66a544075dc3bd70133e3ca917cb6
WallabyG <ycm1366@gmail.com>
2022-01-07 01:28:22 +0900

    fix: Make response user nullable

--- waflog/src/main/kotlin/com/wafflestudio/waflog/global/auth/dto/VerificationTokenPrincipalDto.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/global/auth/dto/VerificationTokenPrincipalDto.kt
@@ -4,11 +4,11 @@ import com.wafflestudio.waflog.domain.user.dto.UserDto
 import com.wafflestudio.waflog.global.auth.model.VerificationTokenPrincipal
 
 data class VerificationTokenPrincipalDto(
-    val user: UserDto.SimpleResponse,
+    val user: UserDto.SimpleResponse?,
     val token: String
 ) {
     constructor(verificationTokenPrincipal: VerificationTokenPrincipal, jwtToken: String) : this(
-        user = UserDto.SimpleResponse(verificationTokenPrincipal.user),
+        user = verificationTokenPrincipal.user?.let { UserDto.SimpleResponse(it) },
         token = jwtToken
     )
 }

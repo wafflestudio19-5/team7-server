4646bbb91210b19d7e4243817fcafdcb95b657d7
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-01-06 23:24:49 +0900

    [#64] series

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/user/repository/SeriesRepository.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/user/repository/SeriesRepository.kt
@@ -1,8 +1,13 @@
 package com.wafflestudio.waflog.domain.user.repository
 
 import com.wafflestudio.waflog.domain.user.model.Series
+import com.wafflestudio.waflog.domain.user.model.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface SeriesRepository : JpaRepository<Series, Long?> {
     fun findByName(name: String): Series?
+
+    @Query("SELECT s FROM Series s WHERE s.name =: name AND s.user =: user")
+    fun findByNameAndUser(name: String, user: User): Series?
 }
---
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/user/service/UserService.kt
@@ -0,0 +1,24 @@
+package com.wafflestudio.waflog.domain.user.service
+
+import com.wafflestudio.waflog.domain.user.dto.SeriesDto
+import com.wafflestudio.waflog.domain.user.exception.SeriesExistException
+import com.wafflestudio.waflog.domain.user.model.Series
+import com.wafflestudio.waflog.domain.user.model.User
+import com.wafflestudio.waflog.domain.user.repository.SeriesRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+    private val seriesRepository: SeriesRepository
+) {
+    fun addSeries(createRequest: SeriesDto.CreateRequest, user: User) {
+        val seriesName = createRequest.name
+        seriesRepository.findByNameAndUser(seriesName, user)
+            ?: run {
+                val newSeries = Series(user, seriesName, mutableListOf())
+                seriesRepository.save(newSeries)
+                return
+            }
+        throw SeriesExistException("이미 존재하는 URL입니다.")
+    }
+}

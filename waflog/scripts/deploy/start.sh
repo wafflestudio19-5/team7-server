4e31f9692b2df3e7dd46b95638d195ebc99685d4
WallabyG <ycm1366@gmail.com>
2022-01-07 16:39:22 +0900

    chore: Update CD script
    Specify additional config location

--- waflog/scripts/deploy/start.sh
+++ waflog/scripts/deploy/start.sh
@@ -4,9 +4,13 @@ JAR_PATH=$(ls -t ~/build/*.jar | head -n 1)
 
 JAR_NAME=$(basename $JAR_PATH)
 
+RESOURCES_PATH=/home/ec2-user/deploy/resources
+
 cp ~/build/$JAR_NAME ~/deploy/
 
 echo "[Deploy] : Running new application"
 
-nohup java -jar -Dspring.profiles.active=prod ~/deploy/$JAR_NAME > ~/deploy/nohup.out 2>&1 &
+nohup java -jar -Dspring.profiles.active=prod \
+        -Dspring.config.additional-location=$RESOURCES_PATH/application-oauth.yml \
+        ~/deploy/$JAR_NAME > ~/deploy/nohup.out 2>&1 &
 

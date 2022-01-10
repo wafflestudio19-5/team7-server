c1e9e602f1ab01d11bc8fab458f5be323d4a7c52
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-01-10 20:53:24 +0900

    fix: [#131] format

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/image/service/ImageService.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/image/service/ImageService.kt
@@ -8,7 +8,6 @@ import com.wafflestudio.waflog.domain.image.repository.ImageRepository
 import com.wafflestudio.waflog.domain.user.model.User
 import com.wafflestudio.waflog.global.auth.CurrentUser
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
@@ -33,7 +32,7 @@ class ImageService(
         val fileToken = removeRequest.token
         val image = imageRepository.findByEmailAndToken(user.email, fileToken)
             ?: throw ImageNotFoundException("image not found")
-        val folderName =user.email.split("@").first()
+        val folderName = user.email.split("@").first()
         imageRepository.deleteById(image.id)
         s3Service.remove(folderName, fileToken, image.originalName)
     }

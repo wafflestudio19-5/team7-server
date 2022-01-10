0c6bfa04241f6b7c7a1aa3b700d5bf908a079132
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-01-10 13:40:47 +0900

    fix: [#91] componentation

--- waflog/src/main/kotlin/com/wafflestudio/waflog/global/config/AWSConfig.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/global/config/AWSConfig.kt
@@ -8,7 +8,9 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 
+@Component
 @Configuration
 class AWSConfig {
     @Bean

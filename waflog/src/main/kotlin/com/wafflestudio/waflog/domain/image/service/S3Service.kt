fa5f3692a8652d939f3027859241e261cb069648
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-01-10 12:53:26 +0900

    chore: [#91] format

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/image/service/S3Service.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/image/service/S3Service.kt
@@ -1,7 +1,6 @@
 package com.wafflestudio.waflog.domain.image.service
 
 import com.amazonaws.services.s3.AmazonS3
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile

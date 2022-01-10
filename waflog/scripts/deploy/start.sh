f23f0c3d16bd21b0debb0d8bf73c4f589b52c925
hs Jeon <81272473+yeonsumia@users.noreply.github.com>
2022-01-10 13:57:32 +0900

    Update start.sh

--- waflog/scripts/deploy/start.sh
+++ waflog/scripts/deploy/start.sh
@@ -12,5 +12,6 @@ echo "[Deploy] : Running new application"
 
 nohup java -jar -Dspring.profiles.active=prod \
         -Dspring.config.additional-location=$RESOURCES_PATH/application-oauth.yml \
+        -Dspring.config.additional-location=$RESOURCES_PATH/application-aws.yml \
         ~/deploy/$JAR_NAME > ~/deploy/nohup.out 2>&1 &
 

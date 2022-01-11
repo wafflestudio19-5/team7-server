2f58eae59a6706381c731f5775e0c08a5324fd86
nemga123 <hspark0419@naver.com>
2022-01-11 15:08:59 +0900

    REFACTOR: #133 error code refactoring

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/post/exception/InvalidPostTitleException.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/post/exception/InvalidPostTitleException.kt
@@ -3,5 +3,5 @@ package com.wafflestudio.waflog.domain.post.exception
 import com.wafflestudio.waflog.global.common.exception.ErrorType
 import com.wafflestudio.waflog.global.common.exception.InvalidRequestException
 
-class InvalidPostTITLEException(detail: String = "") :
+class InvalidPostTitleException(detail: String = "") :
     InvalidRequestException(ErrorType.INVALID_POST_TITLE, detail)

8fb27bb0d4d89d71ad6b53ddd607fa0dca4264cd
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-01-12 18:01:44 +0900

    fix: [#148] moved back repository save logic

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/image/service/ImageService.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/image/service/ImageService.kt
@@ -2,6 +2,7 @@ package com.wafflestudio.waflog.domain.image.service
 
 import com.wafflestudio.waflog.domain.image.dto.ImageDto
 import com.wafflestudio.waflog.domain.image.exception.ImageNotFoundException
+import com.wafflestudio.waflog.domain.image.exception.ImageNotUploadedException
 import com.wafflestudio.waflog.domain.image.exception.InvalidImageFormException
 import com.wafflestudio.waflog.domain.image.model.Image
 import com.wafflestudio.waflog.domain.image.repository.ImageRepository
@@ -24,8 +25,9 @@ class ImageService(
             throw InvalidImageFormException("this format is not allowed to upload")
         val uploadImage = Image(user.userId, fileToken, fileName)
         val folderName = user.userId
-        imageRepository.save(uploadImage)
+
         return s3Service.uploadTo(image, folderName, fileToken, fileName)
+            .also { imageRepository.save(uploadImage) }
     }
 
     fun removeImage(removeRequest: ImageDto.RemoveRequest, @CurrentUser user: User) {
@@ -33,7 +35,8 @@ class ImageService(
         val image = imageRepository.findByUserIdAndToken(user.userId, fileToken)
             ?: throw ImageNotFoundException("image not found")
         val folderName = user.userId
-        imageRepository.deleteById(image.id)
+
         s3Service.remove(folderName, fileToken, image.originalName)
+            .also { imageRepository.deleteById(image.id) }
     }
 }

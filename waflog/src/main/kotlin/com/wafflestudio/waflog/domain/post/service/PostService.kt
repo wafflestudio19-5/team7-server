1142eb8c49c77de84b2090f1717c9af83cd7d253
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-01-17 20:21:42 +0900

    fix: [#174] format

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/post/service/PostService.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/post/service/PostService.kt
@@ -28,7 +28,6 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.util.*
 
 @Service

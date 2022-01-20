97466c3adbf16520e28d2d4f937e2d250976d1c6
WallabyG <ycm1366@gmail.com>
2022-01-20 20:48:02 +0900

    feat: Add email to setting response

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/user/api/UserController.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/user/api/UserController.kt
@@ -109,8 +109,8 @@ class UserController(
     @ResponseStatus(HttpStatus.OK)
     fun getUserSetting(
         @CurrentUser user: User
-    ): UserDto.UserDetailResponse {
-        return UserDto.UserDetailResponse(user)
+    ): UserDto.UserSettingResponse {
+        return UserDto.UserSettingResponse(user)
     }
 
     @PutMapping("/image")

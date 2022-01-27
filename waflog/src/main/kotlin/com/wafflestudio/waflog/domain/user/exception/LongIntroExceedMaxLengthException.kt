3f5050d94f9447e47b96d9a131ba2dd70bacab5d
WallabyG <ycm1366@gmail.com>
2022-01-28 04:51:49 +0900

    style: Match ktlint convention

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/user/exception/LongIntroExceedMaxLengthException.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/user/exception/LongIntroExceedMaxLengthException.kt
@@ -3,6 +3,5 @@ package com.wafflestudio.waflog.domain.user.exception
 import com.wafflestudio.waflog.global.common.exception.ErrorType
 import com.wafflestudio.waflog.global.common.exception.InvalidRequestException
 
-class LongIntroExceedMaxLengthException(detail: String = "")
-    : InvalidRequestException(ErrorType.USER_LONG_INTRO_LENGTH_EXCEED, detail)
-
+class LongIntroExceedMaxLengthException(detail: String = "") :
+    InvalidRequestException(ErrorType.USER_LONG_INTRO_LENGTH_EXCEED, detail)

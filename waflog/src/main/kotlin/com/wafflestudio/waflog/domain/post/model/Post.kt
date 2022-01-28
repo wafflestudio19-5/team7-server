e3af471d3304ad7afd8d77e5d8dd54f08edaac52
WallabyG <ycm1366@gmail.com>
2022-01-28 16:35:40 +0900

    feat: Add post view daily limit

--- waflog/src/main/kotlin/com/wafflestudio/waflog/domain/post/model/Post.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/domain/post/model/Post.kt
@@ -10,6 +10,7 @@ import org.hibernate.annotations.Formula
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
+import javax.persistence.Index
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 import javax.persistence.OneToMany
@@ -21,7 +22,10 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 @Entity
-@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["user_id", "url"])])
+@Table(
+    uniqueConstraints = [UniqueConstraint(columnNames = ["user_id", "url"])],
+    indexes = [Index(columnList = "url")]
+)
 class Post(
 
     @ManyToOne(fetch = FetchType.LAZY)

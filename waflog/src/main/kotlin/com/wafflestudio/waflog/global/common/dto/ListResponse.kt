e89c30afcf9a65a03a783c81afa2c40e1ae9f52c
yeonsumia <81272473+yeonsumia@users.noreply.github.com>
2022-01-29 12:55:05 +0900

    fix: [#209] add value in list response

--- waflog/src/main/kotlin/com/wafflestudio/waflog/global/common/dto/ListResponse.kt
+++ waflog/src/main/kotlin/com/wafflestudio/waflog/global/common/dto/ListResponse.kt
@@ -2,6 +2,7 @@ package com.wafflestudio.waflog.global.common.dto
 
 data class ListResponse<T>(
     val count: Int,
+    val value: Long = 0,
     val contents: List<T>
 ) {
     constructor(list: List<T>?) : this(
